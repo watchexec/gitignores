3bbc8b4de71c13419250874b4865111210bbf8e4
passcod <passcod@users.noreply.github.com>
2021-10-14 13:42:44 +0000

    Auto release 1.0.1

--- src/gitref.rs
+++ src/gitref.rs
@@ -1,6 +1,6 @@
 
 /// The git commit hash of the gitignore repository when this crate was generated.
-pub const GIT_COMMIT_REF: &str = "b0012e4930d0a8c350254a3caeedf7441ea286a3";
+pub const GIT_COMMIT_REF: &str = "cdd9e946da421758c6f42c427c7bc65c8326155d";
 
 /// The BLAKE3 hash of the contents of every file in generator order.
-pub const CONTENT_HASH: &str = "296e8eded0fff91b34838bc453bc2a09344c5bcf66123ff572e565904df6950b";
+pub const CONTENT_HASH: &str = "6724d8735e7b52fca56abd5ad147f9f3a9e1b170762ba860ccde46db129a7593";
