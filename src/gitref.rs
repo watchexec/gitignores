563095376ecf4772a0d906d74e82ad4b591f25c9
passcod <passcod@users.noreply.github.com>
2025-03-11 13:46:38 +0000

    Auto release 2.11.0

--- src/gitref.rs
+++ src/gitref.rs
@@ -1,6 +1,6 @@
 
 /// The git commit hash of the gitignore repository when this crate was generated.
-pub const GIT_COMMIT_REF: &str = "e6b8c0e0df29c05f77a5f76ae551936d6a427622";
+pub const GIT_COMMIT_REF: &str = "03746360179f94ccda3aea942adc8245e4f65a25";
 
 /// The BLAKE3 hash of the contents of every file in generator order.
-pub const CONTENT_HASH: &str = "78fadfe354e7eabc03e6a4f41ce1b58ad73d95c23e4d87111e92b62bf8657f52";
+pub const CONTENT_HASH: &str = "f8e402ba2137ddc11d0bf28b92230f1dc88f214ed16cc1b051bdcd62ab5f283e";
